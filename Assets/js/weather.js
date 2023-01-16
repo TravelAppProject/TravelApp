13ff15c064a5e8c150dd25b4a23ebf5b1697092e
gabess3 <95196142+gabess3@users.noreply.github.com>
2023-01-16 06:38:14 -0600

    added javascript, HTML, and CSS for weather cards and functionality, and searches stored in local storage to be displayed as history buttons which will rerun the function to display the weather once clicked.

---
+++ Assets/js/weather.js
@@ -0,0 +1,113 @@
+var searchInfo = document.querySelector(".loc");
+var searchButton = document.getElementById("submit");
+
+var day1 = document.getElementById("day1");
+var day2 = document.getElementById("day2");
+var day3 = document.getElementById("day3");
+var day4 = document.getElementById("day4");
+var day5 = document.getElementById("day5");
+
+var temp1 = document.getElementById("temp1");
+var temp2 = document.getElementById("temp2");
+var temp3 = document.getElementById("temp3");
+var temp4 = document.getElementById("temp4");
+var temp5 = document.getElementById("temp5");
+
+var desc1 = document.getElementById("description1");
+var desc2 = document.getElementById("description2");
+var desc3 = document.getElementById("description3");
+var desc4 = document.getElementById("description4");
+var desc5 = document.getElementById("description5");
+
+var image1 = document.getElementById('img1');
+var image2 = document.getElementById('img2');
+var image3 = document.getElementById('img3');
+var image4 = document.getElementById('img4');
+var image5 = document.getElementById('img5');
+
+var wind1 = document.getElementById('wind1');
+var wind2 = document.getElementById('wind2');
+var wind3 = document.getElementById('wind3');
+var wind4 = document.getElementById('wind4');
+var wind5 = document.getElementById('wind5');
+
+
+var searchValue = 1;
+var historySearch = document.getElementById("history");
+
+
+function storeSearch() {
+  localStorage.setItem("search " + searchValue, searchInfo.value);
+  var historyButton = document.createElement("button");
+  historyButton.setAttribute("class", "btn");
+  historyButton.textContent = localStorage.getItem("search " + searchValue);
+  historySearch.appendChild(historyButton);
+  searchValue++;
+};
+
+function getWeatherApi() {
+
+  var urlContent = searchInfo.value.replace(/\s/g, "");
+  var requestUrl = "https://api.openweathermap.org/data/2.5/forecast/?q="+ urlContent +"&units=imperial&APPID=5b363f0d85d1d8e70ad27ba598ac067c";
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+
+      for(i=0;i<data.list.length;i++){
+
+        var wicon = "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png";
+        var date = new Date(data.list[i].dt * 1000);
+
+        if(i===0){
+          day1.textContent = date.toDateString();
+          temp1.textContent = "Temp: " + data.list[i].main.temp + "\u00B0 F";
+          wind1.textContent = "Wind: " + data.list[i].wind.speed + " mph";;
+          desc1.textContent = data.list[i].weather[0].description;
+          image1.setAttribute("src", wicon);
+        }
+        if(i===7){
+          day2.textContent = date.toDateString();
+          temp2.textContent = "Temp: " + data.list[i].main.temp + "\u00B0 F";
+          wind2.textContent = "Wind: " + data.list[i].wind.speed + " mph";;
+          desc2.textContent = data.list[i].weather[0].description;
+          image2.setAttribute("src", wicon);
+          }
+        if(i===15){
+          day3.textContent = date.toDateString();
+          temp3.textContent = "Temp: " + data.list[i].main.temp + "\u00B0 F";
+          wind3.textContent = "Wind: " + data.list[i].wind.speed + " mph";;
+          desc3.textContent = data.list[i].weather[0].description;
+          image3.setAttribute("src", wicon);
+        }
+        if(i===23){
+          day4.textContent = date.toDateString();
+          temp4.textContent = "Temp: " + data.list[i].main.temp + "\u00B0 F";
+          wind4.textContent = "Wind: " + data.list[i].wind.speed + " mph";;
+          desc4.textContent = data.list[i].weather[0].description;
+          image4.setAttribute("src", wicon);
+        }
+        if(i===31){
+          day5.textContent = date.toDateString();
+          temp5.textContent = "Temp: " + data.list[i].main.temp + "\u00B0 F";
+          wind5.textContent = "Wind: " + data.list[i].wind.speed + " mph";;
+          desc5.textContent = data.list[i].weather[0].description;
+          image5.setAttribute("src", wicon);
+        }
+      }
+  })
+  storeSearch();
+}
+
+searchButton.addEventListener('click', getWeatherApi);
+
+historySearch.addEventListener("click", function(event){
+  searchInfo.value = event.target.textContent;
+  var removeHistoryButton = event.target;
+  getWeatherApi();
+  removeHistoryButton.remove();
+})
